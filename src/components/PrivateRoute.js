c41afb839b477f9357818aa7fc6adfe26f5b9f19
textshashank <shashankshukla1124@gmail.com>
2023-01-31 00:21:01 +0530

    shashank

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,1 +1,19 @@
-
+import React from "react";
+import Login from "./Login"; 
+const PrivateRoute = (props)=>{
+    
+   
+     if(window.localStorage.getItem("username") && window.localStorage.getItem("password")){
+        return(
+            <>
+              {props.children}
+            </>
+        )
+     }else{
+        return(
+            <Login />
+        )
+     }
+   
+}
+export default PrivateRoute;
